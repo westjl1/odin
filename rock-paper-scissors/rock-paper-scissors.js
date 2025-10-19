ad268e5ec508d6673753e88bd47f8b05563315db
Jeremy West <westjl@comcast.net>
2025-10-18 19:58:17 -0700

    rock paper scissors v1 done. not merge ready, needs testing.

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,10 +1,103 @@
 console.log("I'm running rock-paper-scissors");
 
-function playRound(humanChoice, computerChoice, currentRound) {
-  console.log(`Round ${currentRound}`);
-  console.log(`Human chooses ${humanChoice}`);
-  console.log(`Computer chooses ${computerChoice}`);
+const divGame = document.querySelector("#game-div");
+const ulGameOutput = document.querySelector("#game-output");
 
+const btnRock = document.createElement("button");
+const btnPaper = document.createElement("button");
+const btnScissor = document.createElement("button");
+
+btnRock.textContent = "Rock";
+btnRock.id = "Rock";
+
+btnPaper.textContent = "Paper";
+btnPaper.id = "Paper";
+
+btnScissor.textContent = "Scissors";
+btnScissor.id = "Scissors";
+
+btnRock.addEventListener("click", () => {
+  playRound(btnRock.id);
+});
+btnPaper.addEventListener("click", () => {
+  playRound(btnPaper.id);
+});
+btnScissor.addEventListener("click", () => {
+  playRound(btnScissor.id);
+});
+
+divGame.appendChild(btnRock);
+divGame.appendChild(btnPaper);
+divGame.appendChild(btnScissor);
+
+let round = 1;
+let computerWins = 0;
+let humanWins = 0;
+let ties = 0;
+
+function playRound(humanChoice) {
+  const newLi = document.createElement("li");
+
+  const computerSelection = getComputerChoice();
+  const winner = getWinner(humanChoice, computerChoice);
+
+  newLi.textContent = `Human chooses ${humanChoice} Computer chooses ${computerChoice}`;
+
+  if (winner == "Computer") {
+    console.log("Computer Wins Round!");
+    newLi.textContent += ` Round ${round} winner is ${winner}`;
+    computerWins++;
+    round++;
+  } else if (winner == "Human") {
+    console.log("Human Wins Round");
+    newLi.textContent += ` Round ${round} winner is ${winner}`;
+    humanWins++;
+    round++;
+  } else {
+    console.log("Round ends in a tie, replay round");
+    newLi.textContent = ` Round ${round} ends in tie, replay round`;
+    ties++;
+    // round--;
+  }
+
+  ulGameOutput.appendChild(newLi);
+
+  if (round == 5 || computerWins == 3 || humanWins == 3) {
+    console.log("Game Over");
+    newLi.textContent += ` Game Over`;
+    if (computerWins > humanWins) {
+      console.log(`Computer Wins the Game with ${computerWins}`);
+      console.log(`Human loses the game with ${humanWins} wins`);
+      newLi.textContent += ` Computer Wins the Game with ${computerWins}`;
+    } else {
+      console.log(`Human Wins the Game with ${humanWins} wins`);
+      console.log(`Computer loses the game with ${computerWins} wins`);
+      newLi.textContent += ` Human Wins the Game with ${humanWins}`;
+    }
+    ulGameOutput.appendChild(newLi);
+  }
+}
+
+function getComputerChoice() {
+  const randomChoice = getRandomInt(3);
+  //console.log(randomChoice);
+  switch (randomChoice) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+    default:
+      return "Rock";
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getWinner(humanChoice, computerChoice) {
   //HUMAN ROCK
   if (humanChoice == "Rock" && computerChoice == "Rock") {
     return "Tie";
@@ -32,68 +125,3 @@ function playRound(humanChoice, computerChoice, currentRound) {
     return "Computer";
   }
 }
-
-function getHumanChoice(currentRound) {
-  //return "Rock";
-  return prompt(
-    `Please enter Rock, Paper or Scissors for round ${currentRound}`,
-    ""
-  );
-}
-
-function getComputerChoice() {
-  const randomChoice = getRandomInt(3);
-  //console.log(randomChoice);
-  switch (randomChoice) {
-    case 0:
-      return "Rock";
-    case 1:
-      return "Paper";
-    case 2:
-      return "Scissors";
-    default:
-      return "Rock";
-  }
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function playGame() {
-  let humanWins = 0;
-  let computerWins = 0;
-
-  for (let round = 1; round <= 5; round++) {
-    const humanSelection = getHumanChoice(round);
-    const computerSelection = getComputerChoice(round);
-
-    const winner = playRound(humanSelection, computerSelection, round);
-
-    if (winner == "Computer") {
-      console.log("Computer Wins Round!");
-      computerWins++;
-    } else if (winner == "Human") {
-      console.log("Human Wins Round");
-      humanWins++;
-    } else {
-      console.log("Round ends in a tie, replay round");
-      round--;
-      continue;
-    }
-
-    if (round == 5 || computerWins == 3 || humanWins == 3) {
-      console.log("Game Over");
-      if (computerWins > humanWins) {
-        console.log(`Computer Wins the Game with ${computerWins}`);
-        console.log(`Human loses the game with ${humanWins} wins`);
-      } else {
-        console.log(`Human Wins the Game with ${humanWins} wins`);
-        console.log(`Computer loses the game with ${computerWins} wins`);
-      }
-      break;
-    }
-  }
-}
-
-playGame();
