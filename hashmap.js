30aa443f8070a7a71699f1bcfe8335605c40cb19
Jeremy West <westjl@comcast.net>
2025-12-06 17:34:35 -0800

    hash map complete

--- hashmap.js
+++ hashmap.js
@@ -1,17 +1,17 @@
-function stringToNumber(string) {
-  let hashCode = 0;
-  const primeNumber = 3;
-  for (let i = 0; i < string.length; i++) {
-    // hashCode += string.charCodeAt(i);
-    hashCode = primeNumber * hashCode + string.charCodeAt(i);
-  }
+// function stringToNumber(string) {
+//   let hashCode = 0;
+//   const primeNumber = 3;
+//   for (let i = 0; i < string.length; i++) {
+//     // hashCode += string.charCodeAt(i);
+//     hashCode = primeNumber * hashCode + string.charCodeAt(i);
+//   }
 
-  return hashCode;
-}
+//   return hashCode;
+// }
 
-function hash(name, surname) {
-  return stringToNumber(name) + stringToNumber(surname);
-}
+// function hash(name, surname) {
+//   return stringToNumber(name) + stringToNumber(surname);
+// }
 
 // console.log(stringToNumber("Sara"));
 // console.log(stringToNumber("raSa"));
@@ -28,15 +28,30 @@ class MyHashMap {
   constructor() {
     this.load_factor = 0.75;
     this.capacity = 16;
-    this.buckets = [...Array(this.capacity)];
+    this.buckets = this.buildBuckets(this.capacity);
+  }
+
+  buildBuckets() {
+    const retArray = Array.from({ length: this.capacity }, () => {
+      return new Array();
+    });
+    return retArray;
+  }
+
+  checkCapacity() {
+    const currentLength = this.length();
+
+    if (currentLength >= Math.floor(this.capacity * this.load_factor)) {
+      return true;
+    }
+
+    return false;
   }
 
   hash(key) {
     let hashCode = 0;
 
-    // const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      //   hashCode = key.charCodeAt(i) % this.capacity;
       hashCode = key.charCodeAt(i);
     }
 
@@ -44,69 +59,177 @@ class MyHashMap {
   }
 
   bucket(key) {
-    let simpleHash = hash(key);
-    // return this.buckets[simpleHash];
-    return this.buckets[simpleHash % this.capacity];
+    const simpleHash = this.hash(key);
+    const bucketIndex = simpleHash % this.capacity;
+
+    if (bucketIndex < 0 || bucketIndex >= this.capacity) {
+      throw new Error("Trying to access index out of bounds");
+    }
+
+    const thisBucket = this.buckets[bucketIndex];
+    return thisBucket;
   }
 
   bucketItemContent(bucket, key) {
-    for (content of bucket) {
-      if (content.key == key) {
-        return content;
+    if (bucket && bucket.length > 0) {
+      for (let obj of bucket) {
+        if (obj.key == key) {
+          return obj;
+        }
       }
     }
+
     return null;
   }
 
   set(key, value) {
-    currentBucket = this.bucket(key);
-    checkBucketItemContent = this.bucketItemContent(currentBucket, key);
+    if (this.checkCapacity()) {
+      const currentEntries = this.entries();
+      const newCapacity = this.capacity * 2;
+      this.capacity = newCapacity;
+      this.buckets = this.buildBuckets(newCapacity);
+      currentEntries.forEach((entry) => {
+        this.set(entry.key, entry.value);
+      });
+    }
+
+    let currentBucket = this.bucket(key);
+    let checkBucketItemContent = this.bucketItemContent(currentBucket, key);
     if (checkBucketItemContent) {
       checkBucketItemContent.value = value;
       return;
     }
+
     currentBucket.push({ key, value });
+  }
+
+  get(key) {
+    let currentBucket = this.bucket(key);
+    let checkBucketItemContent = this.bucketItemContent(currentBucket, key);
+    if (checkBucketItemContent) {
+      return checkBucketItemContent.value;
+    }
+    return null;
+  }
+
+  remove(key) {
+    if (this.has(key)) {
+      const currentBucket = this.bucket(key);
+
+      const newBucket = currentBucket.filter((obj) => obj.key != key);
+
+      const simpleHash = this.hash(key);
+      const bucketIndex = simpleHash % this.capacity;
+
+      if (bucketIndex < 0 || bucketIndex >= this.capacity) {
+        throw new Error("Trying to access index out of bounds");
+      }
+
+      this.buckets[bucketIndex] = newBucket;
+    }
+  }
+
+  clear() {
+    this.buckets = this.buildBuckets();
+  }
+
+  has(key) {
+    const currentBucket = this.bucket(key);
+    const checkBucketItemContent = this.bucketItemContent(currentBucket, key);
+    if (checkBucketItemContent) {
+      return true;
+    }
+    return false;
+  }
+
+  length() {
+    let itemCount = 0;
+    this.buckets.forEach((bucket) => {
+      if (bucket && bucket.length > 0) {
+        bucket.forEach((item) => {
+          if (item) {
+            itemCount++;
+          }
+        });
+      }
+    });
+    return itemCount;
+  }
+
+  keys() {
+    let keys = [];
+    this.buckets.forEach((bucket) => {
+      if (bucket && bucket.length > 0) {
+        bucket.forEach((item) => {
+          if (item.key) {
+            keys.push(item.key);
+          }
+        });
+      }
+    });
+    return keys;
+  }
+
+  values() {
+    let values = [];
+    this.buckets.forEach((bucket) => {
+      if (bucket && bucket.length > 0) {
+        bucket.forEach((item) => {
+          if (item.value) {
+            values.push(item.value);
+          }
+        });
+      }
+    });
+    return values;
+  }
+
+  entries() {
+    let entries = [];
+
+    this.buckets.forEach((bucket) => {
+      if (bucket && bucket.length > 0) {
+        bucket.forEach((item) => {
+          if (item) {
+            entries.push({ ...item });
+          }
+        });
+      }
+    });
+
+    return entries;
   }
 }
 
 const myNewHashMap = new MyHashMap();
+myNewHashMap.set("apple", "red");
+myNewHashMap.set("banana", "yellow");
+myNewHashMap.set("carrot", "orange");
+myNewHashMap.set("dog", "brown");
+myNewHashMap.set("elephant", "gray");
+myNewHashMap.set("frog", "green");
+myNewHashMap.set("grape", "purple");
+myNewHashMap.set("hat", "black");
+myNewHashMap.set("ice cream", "white");
+myNewHashMap.set("jacket", "blue");
+myNewHashMap.set("kite", "pink");
+myNewHashMap.set("lion", "golden");
+
+myNewHashMap.set("apple", "green");
+
+myNewHashMap.set("moon", "silver");
+
+myNewHashMap.remove("dog");
+
+// console.log(myNewHashMap.has("cat"));
+
+// console.log(myNewHashMap.length());
+// console.log(myNewHashMap.keys());
+// console.log(myNewHashMap.values());
+console.log(myNewHashMap.entries());
+
+myNewHashMap.clear();
+
+console.log(myNewHashMap.entries());
+
 console.log("Yep, still running");
-
-class HashMap {
-  constructor() {
-    this.buckets = [[], [], []];
-  }
-
-  bucket(key) {
-    let mm3Hash = murmur3(key);
-    return this.buckets[mm3Hash % this.buckets.length];
-  }
-
-  entry(bucket, key) {
-    for (let e of bucket) {
-      if (e.key === key) {
-        return e;
-      }
-    }
-    return null;
-  }
-
-  set(key, value) {
-    let b = this.bucket(key);
-    let e = this.entry(b, key);
-    if (e) {
-      e.value = value;
-      return;
-    }
-    b.push({ key, value });
-  }
-
-  get(key) {
-    let b = this.bucket(key);
-    let e = this.entry(b, key);
-    if (e) {
-      return e.value;
-    }
-    return null;
-  }
-}
